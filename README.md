d53c3d0bccf60db6c46e817d680bf7afe147b524
Abidemi <Anthony.adeniyi@yahoo.com>
2021-04-17 17:26:00 +0100

    remote merged

--- README.md
+++ README.md
@@ -10,6 +10,39 @@ $  npm install react-store-manager -save
 ```
 
 ## Examples
+To import in a react application
+``` es6
+import {StoreManager} from "react-persistent-store-manager";
+
+```
+
+To Set and read a store 
+
+``` es6
+const Store= StoreManager("UserStore");
+
+     /** use this to update a particular store */
+    Store.update("email", "slimkoko.com");
+    Store.update("user", "Ademola Salami");
+
+    /** read a non persistent store data
+     * data will be lost on refresh
+     */
+     const user= Store.useState("user");
+
+    /** 
+     * use this async method to get value that will persist
+     **/
+    const [email, setEmail]= useState(null);
+    Store.useStateAsync("email").then(value=>{
+        setEmail(value)
+    });
+
+```
+
+To run the code
+
+
 
 
 ## Contributions
--- Store.js
+++ Store.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {Store} from "pullstate";
 
 /**
@@ -25,47 +24,9 @@ export const Stores = {
     };
 
 /**
- * Define a global store that cobtains all our substores
+ * Define a global store that contains all our sub-stores
  * The `StoreManager` will handle the individual store update and makes them persistent
  */
  export const AppStore = new Store({
      ...Stores
  });
-
-
-=======
-import {Store} from "pullstate";
-
-/**
- * An abject containing all the stores in our application
- */
-export const Stores = {
-
-    /** the user details store */
-    UserStore:{    
-        name: null,
-        email: null,
-        userType: null
-        },
-
-        /** the app settings store */
-        AppSettingsStore: {
-            measurements: [],
-            userTypes: []
-        }
-
-        /** add any other store here. Must follow the above pattern
-         * you can use any variable type
-         */
-    };
-
-/**
- * Define a global store that cobtains all our substores
- * The `StoreManager` will handle the individual store update and makes them persistent
- */
- export const AppStore = new Store({
-     ...Stores
- });
-
-
->>>>>>> 1d143d7674a1a4c7936dbb1bdb41e0d12976c8a5
